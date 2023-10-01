f64bdb1a40208993190e49aeb79b6bc75e8b3cb7
chris <christopherbartholomewokeke@gmail.com>
2023-10-01 14:03:56 +0100

    Division-D UI completed.

---
+++ app/src/main/java/kodecamp/android/na_my_work/bottomnav_fragments/CategoriesFragment.kt
@@ -0,0 +1,19 @@
+package kodecamp.android.na_my_work.bottomnav_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kodecamp.android.na_my_work.R
+
+class CategoriesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_categories, container, false)
+    }
+
+}
