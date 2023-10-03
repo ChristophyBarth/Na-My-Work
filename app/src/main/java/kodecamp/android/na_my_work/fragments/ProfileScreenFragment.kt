8872a1f424eec946a5e15805ec7628a6c44125d1
Techsultan <salamsultan09@gmail.com>
2023-10-03 08:28:25 +0100

    renamed file name

--- app/src/main/java/kodecamp/android/na_my_work/fragments/ProfileScreenFragment.kt
+++ app/src/main/java/kodecamp/android/na_my_work/fragments/ProfileScreenFragment.kt
@@ -8,14 +8,14 @@ import android.view.ViewGroup
 import kodecamp.android.na_my_work.R
 
 
-class ProfileFragment : Fragment() {
+class ProfileScreenFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_profile_screen, container, false)
     }
 
 
