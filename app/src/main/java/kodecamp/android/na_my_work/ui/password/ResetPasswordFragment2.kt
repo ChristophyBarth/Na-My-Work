d007f6d01c05f7273d6d7b8faa92a9ae608104e8
Omolara Idowu <omolaraidowu44@gmail.com>
2023-10-06 05:48:33 +0100

    -Refactor code -Update the HomeFragment, ProfileScreen, CurrencyFragment, and ChangePasswordFragment

---
+++ app/src/main/java/kodecamp/android/na_my_work/ui/password/ResetPasswordFragment2.kt
@@ -0,0 +1,21 @@
+package kodecamp.android.na_my_work.ui.password
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kodecamp.android.na_my_work.R
+
+class ResetPasswordFragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reset_password2, container, false)
+    }
+
+
+}
